90d2362edfe03089911a540070a83f9334bf950d
Kurmanbek <kumakaraev01@gmail.com>
2020-09-10 01:02:04 -0700

    rep works

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Blueprint
 from flask_restful import Api
 from resources.Hello import Hello
+from resources.Authentication import Login
 
 api_bp = Blueprint('api', __name__)
 api = Api(api_bp)
 
 # Route
-api.add_resource(Hello, '/Hello')
+api.add_resource(Hello, '/hello')
+api.add_resource(Login, '/auth/login')
---
+++ resources/Authentication.py
@@ -0,0 +1,9 @@
+from flask_restful import Resource, reqparse
+class Login(Resource):
+    def __init__(self):
+            self.parser = reqparse.RequestParser()
+            self.parser.add_argument('email', help = 'This field cannot be blank', required = True)
+            self.parser.add_argument('password', help = 'This field cannot be blank', required = True)
+    def post(self):
+        data = self.parser.parse_args()
+        return {"message": "auth"}
