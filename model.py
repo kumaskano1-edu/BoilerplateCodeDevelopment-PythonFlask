ac0bc2357b48abd70abc696b8ffd3d42ee0e5c8d
Kurmanbek <kumakaraev01@gmail.com>
2020-09-13 23:30:27 -0700

    traditional auth works with tokens and creating the user table

--- model.py
+++ model.py
@@ -9,22 +9,48 @@ from flask_bcrypt import Bcrypt
 
 ma = Marshmallow()
 db = SQLAlchemy()
-class User(db.Model):
-    __tablename__ = 'users'
+class Column(db.Model):
+    __abstract__ = True
+    def save(self):
+        db.session.add(self)
+        db.session.commit()          
+class TraditionalAuth(Column):
+    __tablename__ = 'traditional_auth'
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
     email = db.Column(db.String(256), nullable=False, unique=True)
-    name = db.Column(db.String(240), nullable=False)
     password = db.Column(db.String(256), nullable=False)
-
-    def __init__(self, email, name, password):
+    def __init__(self, email, password):
         self.email = email
-        self.name = name
-        self.password = Bcrypt().generate_password_hash(password).decode()        
-    def password_is_valid(self, password):
-        return Bcrypt().check_password_hash(self.password, password)
+        self.password = Bcrypt().generate_password_hash(password).decode()
+    def getId(self):
+        return self.id
     @classmethod
     def find_by_email(self, email):
         return self.query.filter_by(email = email).first()
-    def save(self):
-        db.session.add(self)
-        db.session.commit()
+    def password_is_valid(self, password):
+        return Bcrypt().check_password_hash(self.password, password)    
+class SocialAuth(Column):    
+    __tablename__ = 'social_auth'
+    id = db.Column(UUID(as_uuid=True), default=uuid.uuid4, primary_key=True, nullable=False)
+    social_id = db.Column(db.String(230), nullable=False, unique=True)
+    provider_id = db.Column(db.Integer, nullable=False)
+    provider_name = db.Column(db.String(100), nullable=False)
+    def __init__(self, social_id, provider_id, provider_name):
+        self.social_id = social_id
+        self.provider_id = provider_id
+        self.provider_name = provider_name
+class User(Column):
+    __tablename__ = 'users'
+    id = db.Column(db.String(230), nullable=False, primary_key = True)
+    name = db.Column(db.String(240), nullable=False)
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+    def password_is_valid(self, password):
+        return Bcrypt().check_password_hash(self.password, password)
+    @classmethod
+    def find_by_id(self, id):
+        return self.query.filter_by(id = id).first()
+    def serialize(self):
+        return {"id": str(self.id), "name": self.name}
+
--- resources/Authentication.py
+++ resources/Authentication.py
@@ -1,11 +1,10 @@
 import sys
 from flask_jwt_extended import (create_access_token, create_refresh_token, jwt_required, jwt_refresh_token_required, get_jwt_identity, get_raw_jwt)
 from flask_restful import Resource, reqparse
-from src2.model import User
+from src2.model import User, TraditionalAuth, SocialAuth
 from src2.util.errorsAndMessages import error, succefullAuthMessage
 
-#Initializers and Global Constants
-
+#Initializers and Global Constant
 class UserLogin(Resource):
     def __init__(self):
             self.parser = reqparse.RequestParser()
@@ -29,18 +28,22 @@ class UserRegistration(Resource):
             self.parser.add_argument('password', help = 'This field cannot be blank', required = True)
     def post(self):
         data = self.parser.parse_args()
-        user = User(
-            email = data['email'],
-            name = data['name'],
-            password = data['password']
-        )
+        input_email = data['email']
+        input_pass = data['password']
+        input_name = data['name']
 
-        if(User.query.filter_by(email = user.email).first()):
-            return error(500, "Email already exists")
+        if TraditionalAuth.find_by_email(data['email']):
+            return error(500, "User with such an email already exists")
+        authenticationSession = TraditionalAuth(
+            email = input_email,
+            password = input_pass
+        )
         try:
+            authenticationSession.save()
+            user = User(str(authenticationSession.getId()), input_name)
             user.save()
-            access_token = create_access_token(identity = data['email'])
-            refresh_token = create_refresh_token(identity = data['email'])
+            access_token = create_access_token(identity = user.id)
+            refresh_token = create_refresh_token(identity = user.id)
             return(succefullAuthMessage('User Created Sucesfully', access_token, refresh_token))
         except:
             print(error(500, sys.exc_info()[0]))
