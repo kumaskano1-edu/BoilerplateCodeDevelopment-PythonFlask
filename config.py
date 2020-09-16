ed2d5331c960d4f7a1ada14caae877c86b6cf74f
Kurmanbek <kumakaraev01@gmail.com>
2020-09-16 16:11:53 -0700

    seems like

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 SQLALCHEMY_ECHO = False
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-SQLALCHEMY_DATABASE_URI = "postgresql://kurmanbekk:12345678kkm@localhost/classmates"
+SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 JWT_SECRET_KEY = 'kumakr'
