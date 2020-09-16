b6f16ce40bab64613e1eaec192771e7ba2f3ac1d
Kurmanbek <kumakaraev01@gmail.com>
2020-09-16 16:36:01 -0700

    ssss

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-class Config(Object):
+class Config(object):
     SQLALCHEMY_ECHO = False
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
